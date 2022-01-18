6ac376f8127b533ba6a821c103dd580bd220e0de
sam110386 <vinodk110386@gmail.com>
2022-01-19 00:51:21 +0530

    updates

---
+++ database/migrations/2019_11_09_000003_create_add_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddUsersTable extends Migration
+{
+    public function up()
+    {
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+
+                $table->string('email')->unique();
+
+                $table->datetime('email_verified_at')->nullable();
+
+                $table->string('password');
+
+                $table->string('remember_token')->nullable();
+
+                $table->timestamps();
+
+                $table->softDeletes();
+            });
+        }
+    }
+}
